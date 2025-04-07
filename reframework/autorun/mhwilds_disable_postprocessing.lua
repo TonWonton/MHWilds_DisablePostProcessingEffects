f8c6bafe2491d01b3b34f451b60fab37857d3425
TonWonton <154277764+TonWonton@users.noreply.github.com>
2025-04-07 16:58:58 +0200

    Update to v1.3.0

--- reframework/autorun/mhwilds_disable_postprocessing.lua
+++ reframework/autorun/mhwilds_disable_postprocessing.lua
@@ -2,7 +2,7 @@
 local mod = {
     name = "Disable Post Processing Effects",
     id = "DisablePostProcessingEffects",
-    version = "1.2.1",
+    version = "1.3.0",
     author = "TonWonton",
     settings
 }
@@ -12,7 +12,6 @@ local settings =
 {
     TAA = false,
     jitter = false,
-    LDRPostProcessEnable = true,
     colorCorrect = true,
     lensDistortionEnable = false,
     localExposure = true,
@@ -31,7 +30,7 @@ local settings =
     lowerLimit = 0.0,
     upperLimit = 1.0,
     lowerLimitOverlay = 0.0,
-    upperLimitOverlay = 1.0,
+    upperLimitOverlay = 1.0
 }
 
 mod.settings = settings
@@ -43,14 +42,14 @@ local TAAStrength = statics.generate("via.render.ToneMapping.TemporalAA", true)
 local localExposureType = statics.generate("via.render.ToneMapping.LocalExposureType", true)
 local lensDistortionSetting = statics.generate("via.render.RenderConfig.LensDistortionSetting", true)
 
-local apply = false
-local save = false
-local initialized = false
-
 --Singleton and manager types
 local cameraManager, cameraManagerType, graphicsManager, graphicsManagerType
 --gameobject, component, and type definitions
 local camera, cameraGameObject, LDRPostProcess, colorCorrectComponent, tonemapping, tonemappingType, graphicsSetting, displaySettings
+
+local apply = false
+local initialized = false
+local loadDefaults = false
 
 
 --Saves settings to json
@@ -58,9 +57,20 @@ local function SaveSettings()
     json.dump_file("mhwi_remove_postprocessing.json", settings)
 end
 
---Loads settings from json and puts into settings table
-local function LoadSettings()
-    local loadedTable = json.load_file("mhwi_remove_postprocessing.json")
+--Loads settings depending on overload and puts into table
+--0 and default = saved user config or script defaults, 1 = script defaults, 2 = game defaults
+local function LoadSettings(setting)
+    local loadedTable
+    if setting == 1 then
+        loadDefaults = true
+        loadedTable = json.load_file("mhwi_disablepostprocessingeffects_defaults.json")
+    elseif setting == 2 then
+        loadDefaults = true
+        loadedTable = json.load_file("mhwi_disablepostprocessingeffects_game_defaults.json")
+    else
+        loadedTable = json.load_file("mhwi_remove_postprocessing.json")
+    end
+
     if loadedTable ~= nil then
         for key, val in pairs(loadedTable) do
             settings[key] = loadedTable[key]
@@ -91,13 +101,13 @@ end
 local function ApplySettings()
     if initialized == false then log.info("[DISABLE POST PROCESSING] Not initialized, not applying settings") return end
 
-    --Set tonemapping and LDRPostProcessing
+    --Set tonemapping
     tonemapping:call("setTemporalAA", settings.TAA and TAAStrength.Manual or TAAStrength.Disable)
     tonemapping:call("set_EchoEnabled", settings.jitter)
     tonemapping:call("set_EnableLocalExposure", settings.localExposure)
     tonemapping:call("setLocalExposureType", settings.localExposureBlurredLuminance and localExposureType.BlurredLuminance or localExposureType.Legacy)
 
-    --Set contast values depending on customContrastEnable
+    --Set contrast values depending on customContrastEnable
     if settings.customContrastEnable == true then
         tonemapping:call("set_Contrast", settings.customContrast)
     elseif settings.colorCorrect == false then
@@ -106,8 +116,8 @@ local function ApplySettings()
         tonemapping:call("set_Contrast", 0.3)
     end
 
-    --Set gamma and brightness
-    if settings.customBrightnessEnable == true then
+    --Set gamma and brightness depending on customBrightnessEnable and HDR
+    if settings.customBrightnessEnable == true or loadDefaults == true then
         local HDRMode = displaySettings:call("get_HDRMode")
         displaySettings:call("set_UseSDRBrightnessOptionForOverlay", true)
         displaySettings:call("set_Gamma", settings.gamma)
@@ -117,11 +127,13 @@ local function ApplySettings()
         displaySettings:call("set_OutputLowerLimitForOverlay", settings.lowerLimitOverlay)
         displaySettings:call("set_OutputUpperLimitForOverlay", settings.upperLimitOverlay)
         if HDRMode == false then displaySettings:call("updateRequest") end
+        if loadDefaults == true then displaySettings:call("set_UseSDRBrightnessOptionForOverlay", false) end
+        loadDefaults = false
     else
         displaySettings:call("set_UseSDRBrightnessOptionForOverlay", false)
     end
     
-    --Set graphics setting
+    --Set graphics setting and apply if clicked on apply graphics settings button
     graphicsSetting:call("set_Fog_Enable", settings.fog)
     graphicsSetting:call("set_VolumetricFogControl_Enable", settings.volumetricFog)
     graphicsSetting:call("set_FilmGrain_Enable", settings.filmGrain)
@@ -134,8 +146,6 @@ end
 
 --Initialize by getting singletons, types, objects, then create hooks
 local function Initialize()
-    log.info("[DISABLE POST PROCESSING] Trying to initialize...")
-
     --Get singletons managers
     cameraManager = sdk.get_managed_singleton("app.CameraManager")
     if cameraManager == nil then return end
@@ -173,42 +183,62 @@ end
 
 
 --Load settings at the start and keep trying to initialize until all singleton managers get
-LoadSettings()
+LoadSettings(0)
 re.on_frame(function() if initialized == false then Initialize() end end)
+--Save settings at the same time as REFramework
+re.on_config_save(SaveSettings)
 
 
 --Script generated UI
 re.on_draw_ui(function()
-    if imgui.tree_node("Post Processing Settings v1.2.1") then
+    if imgui.tree_node(mod.name .. " v" .. mod.version) then
         local changed = false
 
-        --Save settings when clicking on save box
-        imgui.new_line()
+        --Save settings or load settings when clicking on buttons
         imgui.push_style_color(21, 0xFF030380)
         changed = imgui.small_button("Save settings")
-        imgui.pop_style_color(1)
         if changed == true then SaveSettings() end
+        imgui.pop_style_color(1)
+        imgui.same_line()
+        imgui.push_style_color(21, 0xFF030380)
+        changed = imgui.small_button("Load script defaults")
+        if changed == true then LoadSettings(1) ApplySettings() end
+        imgui.pop_style_color(1)
+        imgui.same_line()
+        imgui.push_style_color(21, 0xFF030380)
+        changed = imgui.small_button("Load game defaults")
+        if changed == true then LoadSettings(2) ApplySettings() end
+        imgui.pop_style_color(1)
+        imgui.push_style_color(21, 0xFF030380)
+        changed = imgui.small_button("Load saved settings")
+        if changed == true then LoadSettings(0) ApplySettings() end
+        imgui.pop_style_color(1)
+        imgui.text("NOTE: requires game restart after loading defaults to fully revert brightness changes")
+        imgui.spacing()
+
         imgui.text("Anti-Aliasing & filters")
-        changed, settings.TAA = imgui.checkbox("TAA enabled", settings.TAA)
+        changed, settings.TAA = imgui.checkbox("TAA", settings.TAA)
         if changed == true then ApplySettings() end
-        changed, settings.jitter = imgui.checkbox("TAA jitter enabled", settings.jitter)
+        imgui.indent(24)
+        changed, settings.jitter = imgui.checkbox("TAA jitter", settings.jitter)
         if changed == true then ApplySettings() end
+        imgui.unindent(24)
         changed, settings.colorCorrect = imgui.checkbox("Color correction", settings.colorCorrect)
         if changed == true then ApplySettings() end
-        changed, settings.localExposure = imgui.checkbox("Local exposure enabled", settings.localExposure)
+        changed, settings.localExposure = imgui.checkbox("Local exposure", settings.localExposure)
         if changed == true then ApplySettings() end
         imgui.indent(24)
         changed, settings.localExposureBlurredLuminance = imgui.checkbox("Use blurred luminance (sharpens)", settings.localExposureBlurredLuminance)
         if changed == true then ApplySettings() end
         imgui.unindent(24)
-        changed, settings.customContrastEnable = imgui.checkbox("Custom contrast enabled", settings.customContrastEnable)
+        changed, settings.customContrastEnable = imgui.checkbox("Enable custom contrast", settings.customContrastEnable)
         if changed == true then ApplySettings() end
         changed, settings.customContrast = imgui.drag_float("Contrast", settings.customContrast, 0.01, 0.01, 5.0)
         if changed == true then ApplySettings() end
         imgui.new_line()
 
         imgui.text("SDR gamma & Brightness")
-        changed, settings.customBrightnessEnable = imgui.checkbox("SDR custom gamma & brightness enabled", settings.customBrightnessEnable)
+        changed, settings.customBrightnessEnable = imgui.checkbox("Enable SDR custom gamma & brightness", settings.customBrightnessEnable)
         if changed == true then ApplySettings() end
         imgui.text("NOTE: requires game restart after disabling to revert changes")
         imgui.spacing()
@@ -239,17 +269,17 @@ re.on_draw_ui(function()
         apply = imgui.small_button("Apply graphics settings")
         imgui.pop_style_color(1)
         if apply == true then ApplySettings() apply = false end
-        changed, settings.lensDistortionEnable = imgui.checkbox("Lens distortion enabled", settings.lensDistortionEnable)
+        changed, settings.lensDistortionEnable = imgui.checkbox("Lens distortion", settings.lensDistortionEnable)
         if changed == true then ApplySettings() end
-        changed, settings.fog = imgui.checkbox("Fog enabled", settings.fog)
+        changed, settings.fog = imgui.checkbox("Fog", settings.fog)
         if changed == true then ApplySettings() end
-        changed, settings.volumetricFog = imgui.checkbox("Volumetric fog enabled", settings.volumetricFog)
+        changed, settings.volumetricFog = imgui.checkbox("Volumetric fog", settings.volumetricFog)
         if changed == true then ApplySettings() end
-        changed, settings.filmGrain = imgui.checkbox("Film grain enabled", settings.filmGrain)
+        changed, settings.filmGrain = imgui.checkbox("Film grain", settings.filmGrain)
         if changed == true then ApplySettings() end
-        changed, settings.lensFlare = imgui.checkbox("Lens flare enabled", settings.lensFlare)
+        changed, settings.lensFlare = imgui.checkbox("Lens flare", settings.lensFlare)
         if changed == true then ApplySettings() end
-        changed, settings.godRay = imgui.checkbox("Godray enabled", settings.godRay)
+        changed, settings.godRay = imgui.checkbox("Godray", settings.godRay)
         if changed == true then ApplySettings() end
         imgui.spacing()
 
